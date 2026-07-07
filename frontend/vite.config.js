511986611fd5a56864abc50c6d89533e2a4f655e
worawit39 <68319010039@loeitech.ac.th>
2026-07-07 11:43:25 +0700

    assss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  // 🚨 ใส่บรรทัดนี้เพิ่มเติมเข้าไปเพื่อแก้ปัญหาหน้าจอขาวบน Nginx
+  base: './',
   server: {
     port: 8080,
     host: true
