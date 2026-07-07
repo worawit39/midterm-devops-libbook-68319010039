e0ce01f63c1c8b2a4930f813f89079f39568f9b0
worawit39 <68319010039@loeitech.ac.th>
2026-07-07 11:24:03 +0700

    as

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// ตั้งค่าให้เสิร์ฟพอร์ตตรงกับที่คุณใช้ใน Docker
 export default defineConfig({
   plugins: [vue()],
   server: {
