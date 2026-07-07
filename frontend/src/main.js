3aafbbef5fafa3ce88983ec4d9524475d14cd591
worawit39 <68319010039@loeitech.ac.th>
2026-07-07 11:13:07 +0700

    ok

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-// สร้าง Instance ของ Vue App โดยดึง Component หลัก (App.vue) เข้ามาใช้งาน
-const app = createApp(App)
-
-// สั่งให้ Vue นำหน้าเว็บไปแสดงผลที่ธาตุ (Element) ที่มี id="app" ในไฟล์ index.html
-app.mount('#app')
